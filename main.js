4c7ef4500c42e7e5f4ba6c598f0b1303b68357f7
chiffonsigh <fredaome@gmail.com>
2016-03-29 23:56:10 -0400

    Trying to add a progress bar to "chop tree"

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// Basic variable declaration - keep track of how many of each
-// item we currently own, and how much the new ones should cost.
+//vars for item count
 var numLogs = 0;
 var incrementLogs = 5;
 
@@ -9,18 +8,27 @@ var incrementPineNeedles = 100;
 var numAxes = 1;
 var durabilityAxes = 100;
 
+//vars for action buttons
+var progressLogs = 0;
+var progressLogsStarted = 0;
+
 var numWidgets = 0;
 var numNoviceWidgeteers = 0;
 var numMasterWidgeteers = 0;
 var noviceWidgeteerCost = 10;
 var masterWidgeteerCost = 25;
 
+//Function to animate a progress bar
+function progress(percent, $element) {
+    var progressBarWidth = percent * $element.width() / 100;
+    //uncomment below line to add a progress % back in
+    //$element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "% ");
+    $element.find('div').animate({ width: progressBarWidth }, .001);
+}
+
 // Increase numWidgets every time produce-widget is clicked
 $('#chop-tree').on('click', function () {
-    numLogs = numLogs + incrementLogs;
-    numPineNeedles = numPineNeedles + incrementPineNeedles;
-    durabilityAxes = durabilityAxes - 1;
-    $("#log").append('<li>'+'You chopped down a tree and got '+ incrementLogs + ' logs and ' + incrementPineNeedles + ' pine needles.' + '</li>');
+	progressLogsStarted = 1;
 });
 
 // Same for novice-widgeteer
@@ -43,6 +51,24 @@ $('#master-widgeteer').on('click', function () {
 
 // Run UI update code every 10ms
 window.setInterval(function () {
+
+	//update progress bars
+	if(progressLogsStarted == 1){
+		while(progressLogs<100){
+			progressLogs = progressLogs + 1;
+			console.log('ProgressLogs: ' + progressLogs)
+			progress(progressLogs, $('#progress-chop-tree'));
+		}
+		progressLogsStarted = 0;
+		console.log(progressLogsStarted);
+		progressLogs = 0;
+		progress(progressLogs, $('#progress-chop-tree'));
+		numLogs = numLogs + incrementLogs;
+	    numPineNeedles = numPineNeedles + incrementPineNeedles;
+	    durabilityAxes = durabilityAxes - 1;
+    	$("#log").prepend('<li>'+'You chopped down a tree and got '+ incrementLogs + ' logs and ' + incrementPineNeedles + ' pine needles.' + '</li>');
+	}
+
     // Novices add 1 per second (1/100 every 10ms)
     numWidgets += (numNoviceWidgeteers * 1 / 100);
 
@@ -55,11 +81,11 @@ window.setInterval(function () {
     $('#axes-count').text(Math.floor(numAxes));
     $('#axes-durability').text(Math.floor(durabilityAxes));
 
-    // Update the widgeteers with their current prices
-    $('#novice-widgeteer').text('Hire Novice Widgeteer - ' + noviceWidgeteerCost);
-    $('#master-widgeteer').text('Hire Master Widgeteer - ' + masterWidgeteerCost);
+    // // Update the widgeteers with their current prices
+    // $('#novice-widgeteer').text('Hire Novice Widgeteer - ' + noviceWidgeteerCost);
+    // $('#master-widgeteer').text('Hire Master Widgeteer - ' + masterWidgeteerCost);
 
-    // Enable/disable the widgeteer buttons based on our numWidgets
-    $('#novice-widgeteer').prop('disabled', noviceWidgeteerCost > numWidgets);
-    $('#master-widgeteer').prop('disabled', masterWidgeteerCost > numWidgets);
+    // // Enable/disable the widgeteer buttons based on our numWidgets
+    // $('#novice-widgeteer').prop('disabled', noviceWidgeteerCost > numWidgets);
+    // $('#master-widgeteer').prop('disabled', masterWidgeteerCost > numWidgets);
 }, 10);
